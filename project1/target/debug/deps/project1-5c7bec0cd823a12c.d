5920edbfeaf41d97de445047726c4c56f197ff91
AnamiRoy <kyou_31@hotmail.com>
2024-12-30 01:41:42 +0900

    test

---
+++ project1/target/debug/deps/project1-5c7bec0cd823a12c.d
@@ -0,0 +1,5 @@
+/root/github/Rust/project1/target/debug/deps/project1-5c7bec0cd823a12c: src/main.rs
+
+/root/github/Rust/project1/target/debug/deps/project1-5c7bec0cd823a12c.d: src/main.rs
+
+src/main.rs:
---
+++ project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+/root/github/Rust/project1/target/debug/project1: /root/github/Rust/project1/src/main.rs
