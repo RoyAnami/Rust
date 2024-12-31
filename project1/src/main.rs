d0c6f23a1c0b1f34fed06a6295fa7aa5f5b7a374
AnamiRoy <kyou_31@hotmail.com>
2025-01-01 00:10:10 +0900

    practice

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,6 +1,14 @@
-fn main(){
-    let mut x = 42;
-    println!("{}", x);
-    x = 13;
-    println!("{}", x);
+fn do_something_that_might_fail(i: i32) -> Result<f32, String> {
+    if i == 42 {
+        Ok(13.0)
+    } else {
+        Err(String::from("正しい値ではありません"))
+    }
+}
+
+fn main() -> Result<(), String> {
+    // コードが簡潔なのに注目！
+    let v = do_something_that_might_fail(432)?;
+    println!("発見 {}", v);
+    Ok(())
 }
--- project1/src/tempCodeRunnerFile.rs
+++ project1/src/tempCodeRunnerFile.rs
@@ -1,6 +1,10 @@
-fn main(){
-    let mut x = 42;
-    println!("{}", x);
-    x = 13;
-    println!("{}" x);
+fn swap(x: i32, y:i32) -> (i32, i32) {
+    ;return (y, x)
+}
+
+fn main() {
+    let result = swap(123, 321);
+    println!("{} {}", result.0, result.1);
+    let (a, b) = swap(result.0, reuslt.1);
+    println!("{} {}", a, b);
 }
